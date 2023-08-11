0c3a6cfad7197c180ebd374f95c448e9d387ce21
endywezy <endywezy@gmail.com>
2023-08-10 19:19:56 -0700

    betty c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,9 +3,10 @@
 /**
  * main - Entry point as the main function
  *
- * Return: Always 0
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
 	puts("\"programming is like building a multilingual puzzle");
 	return (0);
+}
