30ec1a37f4d568dc8da1cc1eca2bdd411023460b
endywezy <endywezy@gmail.com>
2023-08-16 13:26:07 -0700

    putchar

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_alphabet - function print alphabets in lower case
+ * Return: 0
+ */
+
+void print_alphabet(void)
+{
+	char e;
+
+		for (e = 'a' ; e <= 'z' ; e++)
+			_putchar(e);
+	_putchar('\n');
+
+
+}
