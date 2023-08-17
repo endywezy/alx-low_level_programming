e859bf65db58805f772936f8d0f4a5843fc37608
endywezy <endywezy@gmail.com>
2023-08-17 00:15:37 -0700

    putchar

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,1 +1,15 @@
-#
+#include "main.h"
+/**
+ * _slower - checks for lowsecase character
+ * @c: the charaxter to be checked
+ *
+ * Return: 1 if @c is lowercase, 0 otherwise.
+ */
+
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+		return (1);
+	else
+		return (0);
+}
