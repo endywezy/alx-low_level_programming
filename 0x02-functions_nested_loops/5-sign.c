b192a476e30cc13cc575f266d1c515c3e60ffd6a
endywezy <endywezy@gmail.com>
2023-08-17 00:41:21 -0700

    putchar

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,9 +1,10 @@
 #include "main.h"
 /**
  * print_sign - print sign of a number
- * @n: function parameter 
+ * @n: function parameter
  * Return: 1, 0 or -1
  */
+
 int print_sign(int n)
 {
 	if (n > 0)
