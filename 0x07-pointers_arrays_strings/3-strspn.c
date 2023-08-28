60cd067b7625a559dc63e5c13e232a889949c0dc
endywezy <endywezy@gmail.com>
2023-08-28 16:11:15 -0700

    strspn

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _strspn - get the length of a prefix substring
+ * @s: String to search from
+ * @accept: string to search
+ * Return: 0
+ */
+
+unsigned int _strspn(char *s, char *accept)
+{
+	int i, j;
+
+	for (i = 0 ; s[i] != '\0' ; i++)
+	{
+		for (i = 0 ; s[i] != accept[j] ; j++)
+		{
+			if (accept[j] == '\0')
+				return (i);
+		}
+	}
+	return (0);
+}
