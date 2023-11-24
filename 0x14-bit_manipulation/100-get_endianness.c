5b0e9cde2c03381328b3e6a655ccd4a7630b0885
endywezy <endywezy@gmail.com>
2023-11-24 04:48:19 -0800

    update

--- 0x14-bit_manipulation/100-get_endianness.c
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -2,17 +2,17 @@
 
 /**
  * get_endianness - checks if a machine is little or big endian
- * union - A union to interp
+ * union: store int and char
  * Return: 0 for big, 1 for little
  */
 int get_endianness(void)
 {
 	union
 	{
-		int i; 
+		int i;
 		char c;
 	}
 	u;
-	u.i = 1;
-	return (u.c == 1);
+	u . i = 1;
+	return (u . c == 1);
 }
