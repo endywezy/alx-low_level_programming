6b9d35737a00b904a24bfc01df77eedb420b4199
endywezy <endywezy@gmail.com>
2023-11-27 17:55:14 -0800

    file I/O

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,17 +1,16 @@
 #include "main.h"
-#include <unistd.h>
-#include <fcntl.h>
 
 /**
- * create_file - Creates a file for read/write permissions
- * @filename: name of file read
- * @text_content: the text to write to the file
- * close (fd)
- * Repetition of File I/O
- * Return: 1 on success, -1 on failure
+ * * append_text_to_file - appends text to the end of a file
+ * @filename: the name offile to append text into
+ * @text_content: the text to append with NULL termination
+ * Return: 1 on success, -1 on failure, -1 if @filename is NULL, 1 if file
+ * exists, -1 if file doesn't exist
+ *
+ * Repetition of file I/O
+ * close fd then return
  */
-
-int create_file(const char *filename, char *text_content)
+int append_text_to_file(const char *filename, char *text_content)
 {
 	int wr, fd;
 	int len;
@@ -21,17 +20,14 @@ int create_file(const char *filename, char *text_content)
 
 	if (text_content == NULL)
 	{
-		fd = open(filename, O_WRONLY | O_CREAT, 0600);
-		if (fd == -1)
-			return (-1);
 		return (1);
 	}
-	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
+	fd = open(filename, O_WRONLY | O_APPEND);
 	if (fd == -1)
 		return (-1);
-
 	for (len = 0; text_content[len]; len++)
 	{}
+
 	wr = write(fd, text_content, len);
 	if (wr == -1)
 	{
