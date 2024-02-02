444f1d7df53468cfcc2afe9b48365acc94f6977e
endywezy <endywezy@gmail.com>
2024-02-02 04:54:42 -0800

    Hash tables

--- 0x1A-hash_tables/1-djb2.c
+++ 0x1A-hash_tables/1-djb2.c
@@ -2,6 +2,12 @@
 #include <string.h>
 #include <stdio.h>
 #include "hash_tables.h"
+/**
+ * hash_djb2 - implementation of the djb2 algorithm
+ * @str: string used to generate hash value
+ *
+ * Return: hash value
+ */
 unsigned long int hash_djb2(const unsigned char *str)
 {
     unsigned long int hash;
